60f0deceb62ffb4d31611188bed8b3e84f6410e9
AnaghaAvinash <73226336+AnaghaAvinash@users.noreply.github.com>
2020-11-02 19:52:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,12 +7,14 @@ var thickness;
 function setup() {
   createCanvas(1600,400);
   createSprite(400, 200, 10, 10);
-  speed=random(55,90);
-  weight=random(400,1500);
+  
   bullet=createSprite(50,200,100,30);
   bullet.shapeColor="white";
   wall=createSprite(1200,200,thickness,height/2);
   wall.shapeColor=color(80,80,80);
+  speed=random(223,321);
+  thickness=random(22,83);
+  weight=random(30,52);
 }
 
 function draw() {
@@ -42,9 +44,7 @@ function draw() {
 
 
 
-  speed=random(223,321);
-  thickness=random(22,83);
-  weight=random(30,52);
+  
   
 
   drawSprites();
